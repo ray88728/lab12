655131c275d224a2f0e31ff76b23cb1aebb5184f
ray880728 <ray880728@gmail.com>
2020-06-07 16:12:04 +0800

    lab12

--- 12_1_Continuous_Servos_Test/BUILD/K66F/GCC_ARM/main.d
+++ 12_1_Continuous_Servos_Test/BUILD/K66F/GCC_ARM/main.d
@@ -3,16 +3,16 @@ BUILD/K66F/GCC_ARM/main.o: main.cpp BUILD/K66F/GCC_ARM/mbed_config.h \
  /home/ee2405/ee2405/mbed-os/platform/mbed_version.h \
  /home/ee2405/ee2405/mbed-os/rtos/rtos.h \
  /home/ee2405/ee2405/mbed-os/rtos/mbed_rtos_storage.h \
- /home/ee2405/ee2405/mbed-os-build/rtos/source/TARGET_CORTEX/mbed_rtx_storage.h \
- /home/ee2405/ee2405/mbed-os-build/rtos/source/TARGET_CORTEX/rtx5/RTX/Include/rtx_os.h \
- /home/ee2405/ee2405/mbed-os-build/rtos/source/TARGET_CORTEX/rtx5/Include/cmsis_os2.h \
- /home/ee2405/ee2405/mbed-os-build/rtos/source/TARGET_CORTEX/mbed_rtx_conf.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/mbed_rtx.h \
+ /home/ee2405/ee2405/mbed-os/rtos/source/TARGET_CORTEX/mbed_rtx_storage.h \
+ /home/ee2405/ee2405/mbed-os/rtos/source/TARGET_CORTEX/rtx5/RTX/Include/rtx_os.h \
+ /home/ee2405/ee2405/mbed-os/rtos/source/TARGET_CORTEX/rtx5/Include/cmsis_os2.h \
+ /home/ee2405/ee2405/mbed-os/rtos/source/TARGET_CORTEX/mbed_rtx_conf.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/mbed_rtx.h \
  /home/ee2405/ee2405/mbed-os/rtos/Kernel.h \
  /home/ee2405/ee2405/mbed-os/rtos/mbed_rtos_types.h \
  /home/ee2405/ee2405/mbed-os/rtos/Thread.h \
  /home/ee2405/ee2405/mbed-os/rtos/mbed_rtos1_types.h \
- /home/ee2405/ee2405/mbed-os-build/rtos/source/TARGET_CORTEX/rtx4/cmsis_os.h \
+ /home/ee2405/ee2405/mbed-os/rtos/source/TARGET_CORTEX/rtx4/cmsis_os.h \
  /home/ee2405/ee2405/mbed-os/platform/Callback.h \
  /home/ee2405/ee2405/mbed-os/platform/mbed_assert.h \
  /home/ee2405/ee2405/mbed-os/platform/mbed_toolchain.h \
@@ -32,120 +32,120 @@ BUILD/K66F/GCC_ARM/main.o: main.cpp BUILD/K66F/GCC_ARM/mbed_config.h \
  /home/ee2405/ee2405/mbed-os/rtos/Kernel.h \
  /home/ee2405/ee2405/mbed-os/rtos/EventFlags.h \
  /home/ee2405/ee2405/mbed-os/rtos/ConditionVariable.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/nsapi.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/nsapi_types.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/SocketAddress.h \
- /home/ee2405/ee2405/mbed-os-build/platform/mbed_toolchain.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/NetworkStack.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/NetworkInterface.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/nsapi_types.h \
- /home/ee2405/ee2405/mbed-os-build/platform/Callback.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/DNS.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/EthInterface.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/WiFiInterface.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/WiFiAccessPoint.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/CellularBase.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/CellularInterface.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/MeshInterface.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/Socket.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/UDPSocket.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/InternetSocket.h \
- /home/ee2405/ee2405/mbed-os-build/platform/mbed_atomic.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/cmsis.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/fsl_device_registers.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/MK66F18.h \
- /home/ee2405/ee2405/mbed-os-build/cmsis/TARGET_CORTEX_M/core_cm4.h \
- /home/ee2405/ee2405/mbed-os-build/cmsis/TARGET_CORTEX_M/cmsis_version.h \
- /home/ee2405/ee2405/mbed-os-build/cmsis/TARGET_CORTEX_M/cmsis_compiler.h \
- /home/ee2405/ee2405/mbed-os-build/cmsis/TARGET_CORTEX_M/cmsis_gcc.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/system_MK66F18.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/MK66F18_features.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/cmsis_nvic.h \
- /home/ee2405/ee2405/mbed-os-build/platform/cxxsupport/mstd_type_traits \
- /home/ee2405/ee2405/mbed-os-build/platform/cxxsupport/mstd_cstddef \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/nsapi.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/nsapi_types.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/SocketAddress.h \
+ /home/ee2405/ee2405/mbed-os/platform/mbed_toolchain.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/NetworkStack.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/NetworkInterface.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/nsapi_types.h \
+ /home/ee2405/ee2405/mbed-os/platform/Callback.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/DNS.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/EthInterface.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/WiFiInterface.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/WiFiAccessPoint.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/CellularBase.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/CellularInterface.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/MeshInterface.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/Socket.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/UDPSocket.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/InternetSocket.h \
+ /home/ee2405/ee2405/mbed-os/platform/mbed_atomic.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/cmsis.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/fsl_device_registers.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/MK66F18.h \
+ /home/ee2405/ee2405/mbed-os/cmsis/TARGET_CORTEX_M/core_cm4.h \
+ /home/ee2405/ee2405/mbed-os/cmsis/TARGET_CORTEX_M/cmsis_version.h \
+ /home/ee2405/ee2405/mbed-os/cmsis/TARGET_CORTEX_M/cmsis_compiler.h \
+ /home/ee2405/ee2405/mbed-os/cmsis/TARGET_CORTEX_M/cmsis_gcc.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/system_MK66F18.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/MK66F18_features.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/cmsis_nvic.h \
+ /home/ee2405/ee2405/mbed-os/platform/cxxsupport/mstd_type_traits \
+ /home/ee2405/ee2405/mbed-os/platform/cxxsupport/mstd_cstddef \
  /home/ee2405/ee2405/mbed-os/platform/internal/mbed_atomic_impl.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/SocketStats.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/SocketStats.h \
  /home/ee2405/ee2405/mbed-os/platform/SingletonPtr.h \
  /home/ee2405/ee2405/mbed-os/platform/mbed_atomic.h \
  /home/ee2405/ee2405/mbed-os/platform/PlatformMutex.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/SocketAddress.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/SocketAddress.h \
  /home/ee2405/ee2405/mbed-os/hal/ticker_api.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/TARGET_FRDM/device.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/api/objects.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/api/PortNames.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/TARGET_FRDM/PeripheralNames.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/TARGET_FRDM/PinNames.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/drivers/fsl_flash.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/drivers/fsl_common.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/fsl_device_registers.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/drivers/fsl_clock.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/api/dma_api_hal.h \
- /home/ee2405/ee2405/mbed-os-build/hal/dma_api.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/drivers/fsl_edma.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/api/gpio_object.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/us_ticker_defines.h \
- /home/ee2405/ee2405/mbed-os-build/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/drivers/fsl_pit.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/TCPSocket.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/TCPServer.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/TLSSocketWrapper.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/platform.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/inc/mbedtls/config.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/platform/inc/platform_mbed.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/inc/mbedtls/check_config.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/platform_time.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/inc/mbedtls/ssl.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/bignum.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/ecp.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/inc/mbedtls/ssl_ciphersuites.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/pk.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/md.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/rsa.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/ecdsa.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/cipher.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/platform_util.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/inc/mbedtls/x509_crt.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/inc/mbedtls/x509.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/asn1.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/inc/mbedtls/x509_crl.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/ecdh.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/entropy.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/sha512.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/ctr_drbg.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/mbed-crypto/inc/mbedtls/aes.h \
- /home/ee2405/ee2405/mbed-os-build/features/mbedtls/inc/mbedtls/error.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/DTLSSocketWrapper.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/TLSSocketWrapper.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/TLSSocket.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/DTLSSocket.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/DTLSSocketWrapper.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/UDPSocket.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/nsapi_ppp.h \
- /home/ee2405/ee2405/mbed-os-build/platform/FileHandle.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/TARGET_FRDM/device.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/api/objects.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/api/PortNames.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/TARGET_FRDM/PeripheralNames.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/TARGET_FRDM/PinNames.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/drivers/fsl_flash.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/drivers/fsl_common.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/device/fsl_device_registers.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/drivers/fsl_clock.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/api/dma_api_hal.h \
+ /home/ee2405/ee2405/mbed-os/hal/dma_api.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/drivers/fsl_edma.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/api/gpio_object.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/us_ticker_defines.h \
+ /home/ee2405/ee2405/mbed-os/targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_K66F/drivers/fsl_pit.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/TCPSocket.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/TCPServer.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/TLSSocketWrapper.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/platform.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/inc/mbedtls/config.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/platform/inc/platform_mbed.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/inc/mbedtls/check_config.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/platform_time.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/inc/mbedtls/ssl.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/bignum.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/ecp.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/inc/mbedtls/ssl_ciphersuites.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/pk.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/md.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/rsa.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/ecdsa.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/cipher.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/platform_util.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/inc/mbedtls/x509_crt.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/inc/mbedtls/x509.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/asn1.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/inc/mbedtls/x509_crl.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/ecdh.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/entropy.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/sha512.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/ctr_drbg.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/mbed-crypto/inc/mbedtls/aes.h \
+ /home/ee2405/ee2405/mbed-os/features/mbedtls/inc/mbedtls/error.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/DTLSSocketWrapper.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/TLSSocketWrapper.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/TLSSocket.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/DTLSSocket.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/DTLSSocketWrapper.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/UDPSocket.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/nsapi_ppp.h \
+ /home/ee2405/ee2405/mbed-os/platform/FileHandle.h \
  /home/ee2405/ee2405/mbed-os/platform/mbed_poll.h \
  /home/ee2405/ee2405/mbed-os/platform/platform.h \
- /home/ee2405/ee2405/mbed-os-build/features/netsocket/NetworkStack.h \
+ /home/ee2405/ee2405/mbed-os/features/netsocket/NetworkStack.h \
  /home/ee2405/ee2405/mbed-os/events/mbed_events.h \
  /home/ee2405/ee2405/mbed-os/events/EventQueue.h \
  /home/ee2405/ee2405/mbed-os/events/equeue.h \
  /home/ee2405/ee2405/mbed-os/events/internal/equeue_platform.h \
- /home/ee2405/ee2405/mbed-os-build/rtos/mbed_rtos_storage.h \
+ /home/ee2405/ee2405/mbed-os/rtos/mbed_rtos_storage.h \
  /home/ee2405/ee2405/mbed-os/events/Event.h \
  /home/ee2405/ee2405/mbed-os/events/UserAllocatedEvent.h \
  /home/ee2405/ee2405/mbed-os/events/mbed_shared_queues.h \
- /home/ee2405/ee2405/mbed-os-build/features/storage/filesystem/mbed_filesystem.h \
- /home/ee2405/ee2405/mbed-os-build/features/storage/filesystem/FileSystem.h \
+ /home/ee2405/ee2405/mbed-os/features/storage/filesystem/mbed_filesystem.h \
+ /home/ee2405/ee2405/mbed-os/features/storage/filesystem/FileSystem.h \
  /home/ee2405/ee2405/mbed-os/platform/FileBase.h \
  /home/ee2405/ee2405/mbed-os/platform/FileHandle.h \
  /home/ee2405/ee2405/mbed-os/platform/DirHandle.h \
  /home/ee2405/ee2405/mbed-os/platform/FileSystemLike.h \
  /home/ee2405/ee2405/mbed-os/platform/FileSystemHandle.h \
- /home/ee2405/ee2405/mbed-os-build/features/storage/blockdevice/BlockDevice.h \
- /home/ee2405/ee2405/mbed-os-build/features/storage/filesystem/File.h \
- /home/ee2405/ee2405/mbed-os-build/features/storage/filesystem/Dir.h \
- /home/ee2405/ee2405/mbed-os-build/features/storage/blockdevice/ChainingBlockDevice.h \
- /home/ee2405/ee2405/mbed-os-build/features/storage/blockdevice/BlockDevice.h \
- /home/ee2405/ee2405/mbed-os-build/features/storage/blockdevice/SlicingBlockDevice.h \
- /home/ee2405/ee2405/mbed-os-build/features/storage/blockdevice/HeapBlockDevice.h \
+ /home/ee2405/ee2405/mbed-os/features/storage/blockdevice/BlockDevice.h \
+ /home/ee2405/ee2405/mbed-os/features/storage/filesystem/File.h \
+ /home/ee2405/ee2405/mbed-os/features/storage/filesystem/Dir.h \
+ /home/ee2405/ee2405/mbed-os/features/storage/blockdevice/ChainingBlockDevice.h \
+ /home/ee2405/ee2405/mbed-os/features/storage/blockdevice/BlockDevice.h \
+ /home/ee2405/ee2405/mbed-os/features/storage/blockdevice/SlicingBlockDevice.h \
+ /home/ee2405/ee2405/mbed-os/features/storage/blockdevice/HeapBlockDevice.h \
  /home/ee2405/ee2405/mbed-os/platform/mbed_toolchain.h \
  /home/ee2405/ee2405/mbed-os/platform/platform.h \
  /home/ee2405/ee2405/mbed-os/platform/mbed_application.h \
@@ -196,7 +196,7 @@ BUILD/K66F/GCC_ARM/main.o: main.cpp BUILD/K66F/GCC_ARM/mbed_config.h \
  /home/ee2405/ee2405/mbed-os/platform/mbed_critical.h \
  /home/ee2405/ee2405/mbed-os/platform/CircularBuffer.h \
  /home/ee2405/ee2405/mbed-os/drivers/FlashIAP.h \
- /home/ee2405/ee2405/mbed-os-build/hal/flash_api.h \
+ /home/ee2405/ee2405/mbed-os/hal/flash_api.h \
  /home/ee2405/ee2405/mbed-os/drivers/MbedCRC.h \
  /home/ee2405/ee2405/mbed-os/drivers/internal/TableCRC.h \
  /home/ee2405/ee2405/mbed-os/hal/crc_api.h \
@@ -205,7 +205,7 @@ BUILD/K66F/GCC_ARM/main.o: main.cpp BUILD/K66F/GCC_ARM/mbed_config.h \
  /home/ee2405/ee2405/mbed-os/drivers/ResetReason.h \
  /home/ee2405/ee2405/mbed-os/drivers/Timer.h \
  /home/ee2405/ee2405/mbed-os/drivers/Ticker.h \
- /home/ee2405/ee2405/mbed-os-build/platform/cxxsupport/mstd_utility \
+ /home/ee2405/ee2405/mbed-os/platform/cxxsupport/mstd_utility \
  /home/ee2405/ee2405/mbed-os/drivers/TimerEvent.h \
  /home/ee2405/ee2405/mbed-os/hal/lp_ticker_api.h \
  /home/ee2405/ee2405/mbed-os/drivers/Timeout.h \
